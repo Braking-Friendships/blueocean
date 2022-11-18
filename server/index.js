bbc12779eb7a3f13c68bd13b42ba6de3a9c406f5
Josh <josh@sf-iron.com>
2022-11-18 15:47:02 -0800

    Setup initial socketio on server and client with example emitters and listeners on both sides

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
-const router = require('./routes.js')
 const app = express();
+const httpServer = require('http').createServer(app);
+const options = {
+  cors: ['http://localhost:3000']
+};
+const io = require('socket.io')(httpServer, options);
+const router = require('./routes.js')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', router)
-
-app.listen(5001, () => {
-  console.log('Server started on port 5001')
+io.on('connection', socket => {
+  console.log(socket.id)
+  socket.emit('receive-message', 'hello this is from the server')
+  socket.on('send-message', message => {
+    console.log(message)
+  })
 })
+
+httpServer.listen(5001, () => console.log('Listening on 5001'));
