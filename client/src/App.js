879428b7eef974e07a9101fe8cc695e1c0a1f6dc
Josh <josh@sf-iron.com>
2022-11-18 16:25:40 -0800

    Initial firebase setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,38 @@
 import axios from 'axios';
 import { useEffect } from 'react';
 import { io } from 'socket.io-client';
+import { useAuth } from './contexts/AuthContext';
 
 
 function App() {
-  const socket = io('http://localhost:5001');
+  const [userInfo, setUserInfo] = useState();
 
+  const getUserData = async (user) => {
+    let userData = await axios.get(`/api/login/${user.firebaseId}`, {params: user});
+    if (userData) {
+      setUserInfo(userData.data[0])
+      return userData.data[0];
+    }
+  };
+
+  const createNewUser = async (user) => {
+    let userDataPost = await axios.post(`/api/signup/${user.firebaseId}`, {params: user});
+    if (userDataPost) {
+      let userData = await axios.get(`/api/login/${user.firebaseId}`, {params: user});
+      setUserInfo(userData.data[0]);
+      return userData.data[0];
+    }
+  };
+
+
+
+  const socket = io('http://localhost:5001');
   socket.on('connect', () => {
     console.log(`You connected with id: ${socket.id}`);
   })
-
   socket.on('receive-message', message => {
     console.log(message)
   })
-
   socket.emit('send-message', 'Hello from the client')
 
   return (
---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,65 @@
+import React, { useRef, useState } from 'react';
+import { useAuth } from '../../contexts/AuthContext';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Login = ({ getUserData }) => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const { login, currentUser } = useAuth();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState('');
+  const navigate = useNavigate();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setError('');
+      setLoading(true);
+      const user = await login(emailRef.current.value, passwordRef.current.value);
+      if (user) {
+        const userRole = await getUserData(user);
+        if (userRole) {
+          console.log('logging in as', userRole)
+          if (userRole === "Client") {
+            navigate('/client')
+          } else if (userRole === "Coach") {
+            navigate('/coach')
+          } else {
+            console.log('not loading')
+          }
+        }
+      }
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <form className="w-72 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={onSubmit}>
+        {/* <h2 className="w-48 text-3xl font-bold underline">Log in</h2> */}
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">Email:</label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="email" ref={emailRef} placeholder="email" />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2">Password:</label>
+          <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="password" ref={passwordRef} placeholder="password" />
+        </div>
+        <div className="mb-6 flex items-center justify-between">
+          <button className="bg-[#394D79] hover:bg-[#293757] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" disabled={loading} type="submit">Log in</button>
+          <div className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+            <Link className="" to='/forgot-password'>Forgot password?</Link>
+          </div>
+        </div>
+        <div className="">
+          Need an account? <Link className="font-bold text-sm text-blue-500 hover:text-blue-800" to='/signup'>Sign up</Link>
+        </div>
+        {error && console.log('error', {error})}
+      </form>
+    </div>
+  );
+};
+
+export default Login;
