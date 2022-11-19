c52f6a7fa34e17ea37c3c8730a49dc03f742c51f
Hieu Ngo <hieu.ngo12989@gmail.com>
2022-11-19 15:19:57 -0800

    Created dummyData. uncommented some routes, but no changes yet.

--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,14 @@
 // import db.js when created
-const { User } = require('../db/db.js')
+const { User } = require('../db/db.js');
+// use dummyData
+const dummyData = require('../dummyData.js');
 
 module.exports = {
   createUser: async (user) => {
     try {
       const filter = {firebase_id: user.firebaseId};
-      return await User.findOneAndUpdate(filter, user, {upsert: true})
+      console.log('dummyData: ', dummyData.users);
+      // return await User.findOneAndUpdate(filter, user, {upsert: true})
     } catch (error) {
       return error
     }
