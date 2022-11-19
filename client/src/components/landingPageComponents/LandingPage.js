71240e0a489a14469550fc937eec1136bd908253
Joseph <jredboyz@gmail.com>
2022-11-19 14:52:48 -0800

    add css for landing page

--- client/src/components/landingPageComponents/LandingPage.js
+++ client/src/components/landingPageComponents/LandingPage.js
@@ -1,16 +1,27 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import react from 'react';
-import NavBar from './NavBar.js'
+import img from './tyranitarCropped.png'
+import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
   return (
-    <div>
-      {/* <GameNav /> */}
-      <div>
-        <button>Host a Game</button>
-        <button>Join a Game</button>
-        <button>Join a Random Game</button>
+    <div className='relative top-1/4'>
+      <div className='flex justify-evenly'>
+        {/* <GameNav /> */}
+        <div className='flex flex-col gap-4 justify-around'>
+        {/* bg-[#81B29A] hover:outline text-white font-bold py-2 px-4 rounded */}
+          <Link className='p-4 bg-[#81B29A] rounded h-16 w-80 text-xl text-center text-white font-bold hover:outline hover:outline-4 hover:outline-[#E07A5F] '> Host a Game </Link>
+          <button className='p-2 bg-[#81B29A] rounded w-80 h-16 text-xl text-white font-bold'> Join a Game </button>
+          <button className='p-2 bg-[#81B29A] rounded w-80 h-16 text-xl text-white font-bold'> Join a Random Game </button>
+        </div>
+        {/* <Profile /> */}
+        <div className='flex flex-col gap-4 justify-center'>
+          <img src={img} alt='profile picture' className='object cover h-80 w-80 rounded-full'/>
+          <div className='flex justify-center'>
+            <div className='max-w-max text-2xl'>Username</div>
+          </div>
+        </div>
       </div>
-      {/* <Profile /> */}
     </div>
   )
 }
--- client/src/components/landingPageComponents/NavBar.js
+++ client/src/components/landingPageComponents/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
 return (
     <nav className='flex justify-between text-white bg-[#3D405B] p-3'>
       <a href='/'>Braking Friendships</a>
-      <ul className='flex gap-2'>
+      <ul className='flex gap-3'>
         <li>
           <Link to='/profile'>Profile</Link>
         </li>
