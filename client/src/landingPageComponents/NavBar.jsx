a0bf0b8c8080deb8f5f534955d2f13d374c4164e
Joseph <jredboyz@gmail.com>
2022-11-18 16:04:11 -0800

    some folder structure

---
+++ client/src/landingPageComponents/NavBar.jsx
@@ -0,0 +1,16 @@
+import react from 'react';
+
+
+const NavBar = () => {
+return (
+    <div>
+      this is the NavBar
+      {/* <Profile /> */}
+
+      {/* <Instructions /> */}
+      {/* <Login /> */}
+    </div>
+  )
+}
+
+export default NavBar;
---
+++ client/src/landingPageComponents/main.jsx
@@ -0,0 +1,15 @@
+import react from 'react';
+
+
+const LandingPage = () => {
+  return (
+    <div>
+      this is the landing page
+      {/* <NavBar /> */}
+      {/* <GameNav /> */}
+      {/* <Profile /> */}
+    </div>
+  )
+}
+
+export default LandingPage;
