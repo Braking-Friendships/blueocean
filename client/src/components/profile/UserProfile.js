ab80d926a2eff6dd0b9ea4f787928354799aed2e
Hieu Ngo <hieu.ngo12989@gmail.com>
2022-11-19 11:38:25 -0800

    Added avatar photos

--- client/src/components/profile/UserProfile.js
+++ client/src/components/profile/UserProfile.js
@@ -1,14 +1,17 @@
 import react, { useState, useEffect } from 'react';
 import axios from 'axios';
+import madCat from '../../assets/avatars/madCat.png';
+import happyCat from '../../assets/avatars/happyCat.png';
 
 const UserProfile = ({ socket }) => {
   const [profile, setProfile] = useState({
     username: 'Randy',
     email: 'definitelyNotRandy@isRandy.com',
     friends: [],
-    total_wins: 4,
+    total_wins: 1,
     total_games: 7,
     firebase_id: '2Phajn1cOASIM4jntQkZeAQwLUv2',
+    // avatar: madCat,
   });
 
 
@@ -20,6 +23,9 @@ const UserProfile = ({ socket }) => {
       email: 'definitelyNotRandy@isRandy.com',
       friends: [],
       firebaseId: Math.random().toString(),
+      firstName: 'Randy',
+      lastName: 'Not',
+      avatar: madCat
     }
     await socket.emit('create-user', fakeUserCreation)
     socket.on('send-user-data', data => {
@@ -40,20 +46,27 @@ const UserProfile = ({ socket }) => {
   console.log(profile)
 
   const calculateWinRate = () => {
-    return ((profile.total_wins/profile.total_games) * 100).toFixed(2) + '%';
+    return ((profile.total_wins / profile.total_games) * 100).toFixed(2) + '%';
   }
 
   return (
     <div className="w-screen h-screen bg-[#F4F1DE] flex flex-col justify-center items-center">
       {/* <div> */}
       <h1 onClick={() => createUser()}>USER PROFILE</h1>
-      <br/>
+      <br />
 
-        <h4>PROFILE AVATAR</h4>
-        <h4>{profile.username}</h4>
-        <h4>Number of Wins: {profile.total_wins}</h4>
-        <h4>Total games played: {profile.total_games}</h4>
-        {profile.total_games > 0 && <h4>Win Rate: {calculateWinRate()}</h4>}
+      {!profile.avatar &&
+        <img src={happyCat} className='pointer-events-none w-52 h-auto rounded-full' alt="avatar card" />}
+      {profile.avatar &&
+        <img src={profile.avatar} className='pointer-events-none w-52 h-auto rounded-full' alt="avatar card" />}
+      <br />
+
+      <h4>{profile.username}</h4>
+      <br />
+
+      <h4>Number of Wins: {profile.total_wins}</h4>
+      <h4>Total games played: {profile.total_games}</h4>
+      {profile.total_games > 0 && <h4>Win Rate: {calculateWinRate()}</h4>}
 
     </div>
   );
