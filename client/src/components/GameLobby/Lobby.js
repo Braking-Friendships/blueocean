e8d2ad48f0a8ee4e4b88b5fbab96d80e1c6476bc
Hieu Ngo <hieu.ngo12989@gmail.com>
2022-11-19 16:45:22 -0800

    Minor setup for game lobby, merging from main first

---
+++ client/src/components/GameLobby/Lobby.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { socket, emitters } from '../../socket';
+
+const Lobby = () => {
+
+
+
+
+  return (
+    <div>
+      Game lobby entered
+
+      <div> Game ID</div>
+    </div>
+  );
+};
+
+export default Lobby;
