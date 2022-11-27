2dafd0e34e791c1da1326e29a79616b4bf0d7784
jonahchoi <cjonah227@gmail.com>
2022-11-27 13:49:37 -0800

    Fixed a lot of bugs with game. Add counts to total games/wins. Change gameover screen

--- client/src/components/GameComponents/GameRoom.js
+++ client/src/components/GameComponents/GameRoom.js
@@ -29,7 +29,7 @@ const GameRoom = ({ userInfo }) => {
   return (
     <>
       <div className={isOpen ? 'grid grid-cols-5 bg-[#3D405B]': ""}>
-        <Board myId={userInfo.username} />
+        <Board myId={userInfo.username} userInfo={userInfo} />
         <div className={isOpen ? 'h-170 w-screen max-w-sm flex flex-col flex-grow absolute right-0 px-15 pt-0 pb-0': "hidden"}>
           <button type="button" className="absolute left-0 ml-3 mt-3 text-white" onClick={(e) => {
           setOpen(!isOpen);
